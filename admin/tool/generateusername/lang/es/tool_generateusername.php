b80c5d65806dd5ffa68057924518a076b49b2756
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2025-08-12 08:51:59 -0500

    feat: enhance username generator (#19)

---
+++ admin/tool/generateusername/lang/es/tool_generateusername.php
@@ -0,0 +1,34 @@
+<?php
+// Este archivo forma parte de Moodle - http://moodle.org/
+//
+// Moodle es software libre: puede redistribuirlo y/o modificarlo
+// bajo los términos de la Licencia Pública General GNU publicada por
+// la Free Software Foundation, ya sea la versión 3 de la licencia, o
+// (a su elección) cualquier versión posterior.
+//
+// Moodle se distribuye con la esperanza de que sea útil,
+// pero SIN NINGUNA GARANTÍA; sin incluso la garantía implícita de
+// COMERCIABILIDAD o IDONEIDAD PARA UN PROPÓSITO PARTICULAR. Vea la
+// Licencia Pública General GNU para más detalles.
+//
+// Debería haber recibido una copia de la Licencia Pública General GNU
+// junto con Moodle. Si no, vea <http://www.gnu.org/licenses/>.
+
+/**
+ * Cadenas de idioma para tool_generateusername.
+ *
+ * @package   tool_generateusername
+ * @copyright 2024 Alonso Arias
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 o posterior
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Generar nombres de usuario';
+$string['generate'] = 'Generar nombres de usuario';
+$string['downloadcsv'] = 'Descargar CSV';
+$string['downloadexcel'] = 'Descargar Excel';
+$string['missingrequiredfields'] = 'No se encontraron los campos requeridos firstname, lastname, email, idnumber y password.';
+$string['usernamepresent'] = 'El archivo subido ya contiene una columna username.';
+$string['privacy:metadata'] = 'El plugin Generar nombres de usuario no almacena datos personales.';
+
