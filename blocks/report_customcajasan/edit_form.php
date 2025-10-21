17e72b15a1d6a684b3f3bad26158ef39d214e8b9
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2025-10-20 22:55:05 -0500

    Limit default block scope to host course and add clear buttons

--- blocks/report_customcajasan/edit_form.php
+++ blocks/report_customcajasan/edit_form.php
@@ -24,6 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+require_once(__DIR__ . '/lib.php');
+
 /**
  * Class block_report_customcajasan_edit_form
  *
@@ -37,6 +39,8 @@ class block_report_customcajasan_edit_form extends block_edit_form {
      * @param MoodleQuickForm $mform Form object to add elements to.
      */
     protected function specific_definition($mform) {
+        global $USER, $PAGE;
+
         $mform->addElement('header', 'configheader', get_string('blocksettings', 'block'));
 
         $options = $this->block->get_available_info_options();
@@ -61,6 +65,136 @@ class block_report_customcajasan_edit_form extends block_edit_form {
         );
         $mform->setType('config_custommessage', PARAM_TEXT);
         $mform->addHelpButton('config_custommessage', 'config_custommessage', 'block_report_customcajasan');
+
+        $parentcontext = null;
+        if (!empty($this->block->instance->parentcontextid)) {
+            $parentcontext = context::instance_by_id($this->block->instance->parentcontextid, IGNORE_MISSING);
+        } else if (!empty($this->block->page->context)) {
+            $parentcontext = $this->block->page->context;
+        }
+
+        $iscoursecontext = $parentcontext && $parentcontext->contextlevel === CONTEXT_COURSE;
+
+        $clearelements = [];
+
+        if ($iscoursecontext) {
+            $mform->addElement('header', 'configrestrictions', get_string('config_restriction_heading', 'block_report_customcajasan'));
+            $mform->addHelpButton('configrestrictions', 'config_restriction_heading', 'block_report_customcajasan');
+
+            $courseoptions = [];
+            $courses = get_user_capability_course('block/report_customcajasan:viewreport', $USER->id, true, 'fullname, shortname', 'fullname');
+            foreach ($courses as $course) {
+                $coursecontext = context_course::instance($course->id, IGNORE_MISSING);
+                $courseoptions[$course->id] = format_string($course->fullname, true, ['context' => $coursecontext ?: null]);
+            }
+
+            if (!empty($courseoptions)) {
+                $coursename = 'config_' . block_report_customcajasan::CONFIG_COURSES;
+                $mform->addElement(
+                    'select',
+                    $coursename,
+                    get_string('config_coursefilters', 'block_report_customcajasan'),
+                    $courseoptions,
+                    ['multiple' => true, 'size' => min(10, max(3, count($courseoptions)))]
+                );
+                $mform->addHelpButton($coursename, 'config_coursefilters', 'block_report_customcajasan');
+
+                $clearcoursename = $coursename . '_clear';
+                $mform->addElement(
+                    'button',
+                    $clearcoursename,
+                    get_string('config_clearselection', 'block_report_customcajasan'),
+                    [
+                        'type' => 'button',
+                        'class' => 'btn btn-secondary mt-1 block-report-customcajasan-clear',
+                        'data-action' => 'clear-selection',
+                        'data-target' => $coursename,
+                    ]
+                );
+                $mform->registerNoSubmitButton($clearcoursename);
+                $clearelements[] = $coursename;
+            } else {
+                $mform->addElement('static', 'config_' . block_report_customcajasan::CONFIG_COURSES . '_notice', '', get_string('config_coursefilters_empty', 'block_report_customcajasan'));
+            }
+
+            $categoryoptions = core_course_category::make_categories_list('block/report_customcajasan:viewreport');
+            if (!empty($categoryoptions)) {
+                $categoryname = 'config_' . block_report_customcajasan::CONFIG_CATEGORIES;
+                $mform->addElement(
+                    'select',
+                    $categoryname,
+                    get_string('config_categoryfilters', 'block_report_customcajasan'),
+                    $categoryoptions,
+                    ['multiple' => true, 'size' => min(10, max(3, count($categoryoptions)))]
+                );
+                $mform->addHelpButton($categoryname, 'config_categoryfilters', 'block_report_customcajasan');
+
+                $clearcategoryname = $categoryname . '_clear';
+                $mform->addElement(
+                    'button',
+                    $clearcategoryname,
+                    get_string('config_clearselection', 'block_report_customcajasan'),
+                    [
+                        'type' => 'button',
+                        'class' => 'btn btn-secondary mt-1 block-report-customcajasan-clear',
+                        'data-action' => 'clear-selection',
+                        'data-target' => $categoryname,
+                    ]
+                );
+                $mform->registerNoSubmitButton($clearcategoryname);
+                $clearelements[] = $categoryname;
+            } else {
+                $mform->addElement('static', 'config_' . block_report_customcajasan::CONFIG_CATEGORIES . '_notice', '', get_string('config_categoryfilters_empty', 'block_report_customcajasan'));
+            }
+        }
+
+        if (!empty($clearelements)) {
+            $PAGE->requires->js_init_code(
+                "(function() {\n" .
+                "    var targets = " . json_encode(array_values($clearelements)) . ";\n" .
+                "    if (!targets.length) {\n" .
+                "        return;\n" .
+                "    }\n" .
+                "    document.addEventListener('click', function(event) {\n" .
+                "        var trigger = event.target;\n" .
+                "        while (trigger && trigger !== document) {\n" .
+                "            if (trigger.hasAttribute('data-action') && trigger.getAttribute('data-action') === 'clear-selection') {\n" .
+                "                break;\n" .
+                "            }\n" .
+                "            trigger = trigger.parentElement;\n" .
+                "        }\n" .
+                "        if (!trigger || trigger === document) {\n" .
+                "            return;\n" .
+                "        }\n" .
+                "        var name = trigger.getAttribute('data-target');\n" .
+                "        if (!name || targets.indexOf(name) === -1) {\n" .
+                "            return;\n" .
+                "        }\n" .
+                "        event.preventDefault();\n" .
+                "        var element = document.getElementById('id_' + name);\n" .
+                "        if (!element) {\n" .
+                "            return;\n" .
+                "        }\n" .
+                "        if (element.tagName === 'SELECT') {\n" .
+                "            for (var i = 0; i < element.options.length; i++) {\n" .
+                "                element.options[i].selected = false;\n" .
+                "            }\n" .
+                "            element.value = '';\n" .
+                "        } else {\n" .
+                "            element.value = '';\n" .
+                "        }\n" .
+                "        var changeEvent;\n" .
+                "        if (typeof window.Event === 'function') {\n" .
+                "            changeEvent = new Event('change', {bubbles: true});\n" .
+                "        } else {\n" .
+                "            changeEvent = document.createEvent('Event');\n" .
+                "            changeEvent.initEvent('change', true, true);\n" .
+                "        }\n" .
+                "        element.dispatchEvent(changeEvent);\n" .
+                "    });\n" .
+                "})();"
+            );
+        }
     }
 
     /**
@@ -75,6 +209,14 @@ class block_report_customcajasan_edit_form extends block_edit_form {
             }
             if (isset($this->block->config->custommessage)) {
                 $defaults->config_custommessage = $this->block->config->custommessage;
+            }
+
+            $restrictions = block_report_customcajasan_compute_restrictions($this->block->config);
+            if (!empty($restrictions['courses'])) {
+                $defaults->{'config_' . block_report_customcajasan::CONFIG_COURSES} = $restrictions['courses'];
+            }
+            if (!empty($restrictions['categories'])) {
+                $defaults->{'config_' . block_report_customcajasan::CONFIG_CATEGORIES} = $restrictions['categories'];
             }
         }
 
