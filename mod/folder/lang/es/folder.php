199c7d3ba60882c9806566fc60798e24131acca6
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2025-10-24 11:27:37 -0500

    Refactor folder explorer UI with Moodle components (#177)

---
+++ mod/folder/lang/es/folder.php
@@ -0,0 +1,75 @@
+<?php
+// Este archivo forma parte de Moodle - http://moodle.org/
+//
+// Moodle es software libre: puedes redistribuirlo y/o modificarlo
+// bajo los términos de la GNU General Public License publicada por
+// la Free Software Foundation, ya sea la versión 3 de la licencia o
+// (a tu elección) cualquier versión posterior.
+//
+// Moodle se distribuye con la esperanza de que sea útil,
+// pero SIN NINGUNA GARANTÍA; incluso sin la garantía implícita de
+// COMERCIALIZACIÓN o IDONEIDAD PARA UN PROPÓSITO PARTICULAR.  Consulta la
+// GNU General Public License para obtener más detalles.
+//
+// Deberías haber recibido una copia de la GNU General Public License
+// junto con Moodle.  En caso contrario, consulta <http://www.gnu.org/licenses/>.
+
+/**
+ * Cadenas para el componente 'folder', idioma 'es'.
+ *
+ * @package   mod_folder
+ * @copyright 2024
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 o posterior
+ */
+
+$string['modulename'] = 'Carpeta';
+$string['explorertitle'] = 'Explorador de archivos';
+$string['exploreraddress'] = 'Dirección';
+$string['explorertree'] = 'Panel de navegación';
+$string['explorercontents'] = 'Contenido de la carpeta';
+$string['foldernavigation'] = 'Navegación de carpetas';
+$string['navigationheader'] = 'Navegación';
+$string['navigationcontrols'] = 'Controles de navegación';
+$string['navigationback'] = 'Atrás';
+$string['navigationgoback'] = 'Ir atrás';
+$string['navigationforward'] = 'Adelante';
+$string['navigationgoforward'] = 'Ir adelante';
+$string['navigationup'] = 'Subir';
+$string['navigationgoup'] = 'Subir un nivel';
+$string['folderbreadcrumbs'] = 'Ruta de carpetas';
+$string['searchplaceholder'] = 'Buscar archivos y carpetas...';
+$string['searcharia'] = 'Buscar';
+$string['sortbyname'] = 'Nombre';
+$string['sortbydate'] = 'Fecha de modificación';
+$string['sortbytype'] = 'Tipo';
+$string['sortbysize'] = 'Tamaño';
+$string['sortlabel'] = 'Ordenar por:';
+$string['sortdirectionaria'] = 'Dirección de ordenamiento';
+$string['sortascending'] = 'Ascendente';
+$string['sortdescending'] = 'Descendente';
+$string['filterlabel'] = 'Mostrar:';
+$string['filterall'] = 'Todos los elementos';
+$string['filterfolders'] = 'Solo carpetas';
+$string['filterfiles'] = 'Solo archivos';
+$string['filterimages'] = 'Imágenes';
+$string['filterdocuments'] = 'Documentos';
+$string['filterspreadsheets'] = 'Hojas de cálculo';
+$string['filterpresentations'] = 'Presentaciones';
+$string['filtervideos'] = 'Videos';
+$string['filteraudios'] = 'Audio';
+$string['filterarchives'] = 'Comprimidos';
+$string['filtercode'] = 'Código';
+$string['filterother'] = 'Otros archivos';
+$string['filterresults'] = 'Mostrando {$a->visible} de {$a->total} elementos';
+$string['viewgrid'] = 'Vista de iconos';
+$string['viewlist'] = 'Vista de lista';
+$string['viewdetails'] = 'Vista de detalles';
+$string['tableheadername'] = 'Nombre';
+$string['tableheadertype'] = 'Tipo';
+$string['tableheadersize'] = 'Tamaño';
+$string['tableheaderdatemodified'] = 'Fecha de modificación';
+$string['emptyfolder'] = 'Esta carpeta está vacía';
+$string['noresults'] = 'No hay elementos que coincidan con tu búsqueda';
+$string['clearsearch'] = 'Borrar búsqueda';
+$string['togglefolder'] = 'Desplegar carpeta';
+$string['itemcounts'] = '{$a} elementos';
